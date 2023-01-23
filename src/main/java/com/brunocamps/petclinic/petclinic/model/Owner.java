03cd729bd1e7cc2b7aa839ecb827931c827c9576
Bruno Campos <bruno.mcamps@outlook.com>
2023-01-23 20:21:14 -0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/brunocamps/petclinic/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.brunocamps.petclinic.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/brunocamps/petclinic/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.brunocamps.petclinic.petclinic.model;
+
+public class Vet extends Person{
+
+}
